d1e5ddf6813a678311a45c43415822c27af65cc6
Liubov Naumova <lubochka2005@yandex.ru>
2026-03-31 07:59:43 +0300

    api-final-yatube-ad

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# api_final
-api final
+# API для Yatube
+
+## Описание
+API для социальной платформы Yatube.
+Позволяет работать с публикациями, группами, комментариями и подписками.
+Поддерживает JWT-аутентификацию.
+
+## Технологии
+- Python 3.9+
+- Django 3.2
+- Django REST Framework
+- Djoser
+- SimpleJWT
--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "api",
     "posts",
+    "djoser",
 ]
 
 MIDDLEWARE = [
@@ -90,9 +91,16 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = (BASE_DIR / "static/",)
 
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
-    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.AllowAny",
+    ),
+    "DEFAULT_PAGINATION_CLASS": (
+        "rest_framework.pagination.LimitOffsetPagination",
+    ),
+    "PAGE_SIZE": 10,
 }
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
